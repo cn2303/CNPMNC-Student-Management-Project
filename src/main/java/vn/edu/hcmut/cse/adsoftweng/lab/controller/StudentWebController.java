d289a1d3a49a1a0c5d84042d0c100d7d4c8e8f41
cn2303 <chuongnguyen2335@gmail.com>
2026-02-06 17:20:49 +0700

    Lab 1 Lab 2

---
+++ src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/controller/StudentWebController.java
@@ -0,0 +1,28 @@
+package vn.edu.hcmut.cse.adsoftweng.lab.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller; // Lưu ý: dùng
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import vn.edu.hcmut.cse.adsoftweng.lab.service.StudentService;
+import vn.edu.hcmut.cse.adsoftweng.lab.entity.Student;
+import java.util.List;
+@Controller
+@RequestMapping("/students")
+public class StudentWebController {
+    @Autowired
+    private StudentService service;
+    // Route: GET http://localhost:8080/students
+    @GetMapping
+    public String getAllStudents(Model model) {
+        // 1. Lấy dữ liệu từ Service
+        List<Student> students = service.getAll();
+        // 2. Đóng gói dữ liệu vào "Model" để chuyển sang View
+        // Key "dsSinhVien" sẽ được dùng bên file HTML
+        model.addAttribute("dsSinhVien", students);
+        // 3. Trả về tên của View (không cần đuôi .html)
+        // Spring Boot sẽ tự tìm file tại src/main/resources/templates/students.html
+        return "students.html";
+    }
+}
---
+++ src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/entity/Student.java
@@ -0,0 +1,23 @@
+package vn.edu.hcmut.cse.adsoftweng.lab.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private int age;
+}
